bb5cd6e0118bad71b4eba4ea817ca3a3d9b23a89
FrDarryl <revdjordan@gmail.com>
2016-06-21 01:31:46 -0400

    Tue Jun 21 01:31:46 EDT 2016

--- routes.js
+++ routes.js
@@ -1,17 +1,58 @@
 Router.configure({
-    layoutTemplate: 'layout'
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
+    notFoundTemplate: 'notFound'
 });
 
-Router.route('/', {
-    name: 'home',
-    template: 'home'
+Router.route('home', {
+    path: '/',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'slideshow': {to: 'slideshow'},
+        'footer': {to: 'footer'}
+    }
 });
 
-Router.route('/blog', {
-    name: 'blog',
-//    template: 'blog'
+Router.route('blog', {
+    path: '/blog',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
 });
-Router.route('/resources/documents');
 
-Router.route('/login');
-Router.route('/register');
+Router.route('downloads', {
+    path: '/resources/downloads',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('events', {
+    path: '/events',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('login', {
+    path: '/login',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('register', {
+    path: '/register',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+UI.registerHelper('CurrentRouteName', function() {
+    return Router.current()?Router.current().route.getName():'';
+});
