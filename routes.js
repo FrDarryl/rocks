d8da2056fe1d2da4547204a286f0566ab7e89543
FrDarryl <revdjordan@gmail.com>
2016-06-21 11:57:42 -0400

    Tue Jun 21 11:57:42 EDT 2016

--- routes.js
+++ routes.js
@@ -4,11 +4,35 @@ Router.configure({
     notFoundTemplate: 'notFound'
 });
 
+Router.route('login', {
+    path: '/login',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('register', {
+    path: '/register',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
 Router.route('home', {
     path: '/',
     yieldTemplates: {
         'header': {to: 'header'},
         'slideshow': {to: 'slideshow'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('about', {
+    path: '/about',
+    yieldTemplates: {
+        'header': {to: 'header'},
         'footer': {to: 'footer'}
     }
 });
@@ -21,32 +45,72 @@ Router.route('blog', {
     }
 });
 
-Router.route('downloads', {
-    path: '/resources/downloads',
+Router.route('contact', {
+    path: '/contact',
     yieldTemplates: {
         'header': {to: 'header'},
         'footer': {to: 'footer'}
     }
 });
 
-Router.route('events', {
-    path: '/events',
+Router.route('search', {
+    path: '/search',
     yieldTemplates: {
         'header': {to: 'header'},
         'footer': {to: 'footer'}
     }
 });
 
-Router.route('login', {
-    path: '/login',
+Router.route('liturgyEvents', {
+    path: '/events/liturgies',
     yieldTemplates: {
         'header': {to: 'header'},
         'footer': {to: 'footer'}
     }
 });
 
-Router.route('register', {
-    path: '/register',
+Router.route('meetingEvents', {
+    path: '/events/meetings',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('socialEvents', {
+    path: '/events/socials',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('meetingResources', {
+    path: '/resources/meetings',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('musicResources', {
+    path: '/resources/music',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('newsletterResources', {
+    path: '/resources/newsletters',
+    yieldTemplates: {
+        'header': {to: 'header'},
+        'footer': {to: 'footer'}
+    }
+});
+
+Router.route('rotaResources', {
+    path: '/resources/rotas',
     yieldTemplates: {
         'header': {to: 'header'},
         'footer': {to: 'footer'}
